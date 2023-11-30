7f5ff23de649db7adbd23c06e1e75c692aa08d68
Gilbert Tan <57788135+gbtan1991@users.noreply.github.com>
2023-11-30 14:59:30 +0800

    created the quiz button and question rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,87 @@
-import React from 'react'
-import Quiz from './component/Quiz';
-
-const questions = [
-  
-  {
-    "question": "What does HTML stand for?",
-    "options": [
-      {"option": "Hyper Text Markup Language", "correct": true},
-      {"option": "Hyperlinks and Text Markup Language", "correct": false},
-      {"option": "Home Tool Markup Language", "correct": false}
-    ]
-  },
-  {
-    "question": "Which property is used to change the background color in CSS?",
-    "options": [
-      {"option": "color", "correct": false},
-      {"option": "background-color", "correct": true},
-      {"option": "bgcolor", "correct": false}
-    ]
-  },
-  {
-    "question": "What does CSS stand for?",
-    "options": [
-      {"option": "Counter Style Sheets", "correct": false},
-      {"option": "Computer Style Sheets", "correct": false},
-      {"option": "Cascading Style Sheets", "correct": true}
-    ]
-  },
-  {
-    "question": "Which HTML tag is used to create a hyperlink?",
-    "options": [
-      {"option": "<link>", "correct": false},
-      {"option": "<a>", "correct": true},
-      {"option": "<href>", "correct": false}
-    ]
-  },
-  {
-    "question": "In CSS, which property is used to set the text color of an element?",
-    "options": [
-      {"option": "color", "correct": true},
-      {"option": "text-color", "correct": false},
-      {"option": "font-color", "correct": false}
-    ]
-  }
-]
+import React, { useState, useEffect } from "react";
+import dataQuestions from "./data/questions.json";
 
 export default function App() {
+  
+  const data = [
+    {
+      "id": 1,
+      "question": "What does HTML stand for?",
+      "options": [
+        {"option": "Hyper Text Markup Language", "correct": true},
+        {"option": "Hyperlinks and Text Markup Language", "correct": false},
+        {"option": "Home Tool Markup Language", "correct": false},
+        {"option": "High-level Text Management Language", "correct": false}
+      ]
+    },
+    {
+      "id": 2,
+      "question": "Which property is used to change the background color in CSS?",
+      "options": [
+        {"option": "color", "correct": false},
+        {"option": "background-color", "correct": true},
+        {"option": "bgcolor", "correct": false},
+        {"option": "background", "correct": false}
+      ]
+    },
+    {
+      "id": 3,
+      "question": "What does CSS stand for?",
+      "options": [
+        {"option": "Counter Style Sheets", "correct": false},
+        {"option": "Computer Style Sheets", "correct": false},
+        {"option": "Cascading Style Sheets", "correct": true},
+        {"option": "Colorful Style Sheets", "correct": false}
+      ]
+    },
+    {
+      "id": 4,
+      "question": "Which HTML tag is used to create a hyperlink?",
+      "options": [
+        {"option": "<link>", "correct": false},
+        {"option": "<a>", "correct": true},
+        {"option": "<href>", "correct": false},
+        {"option": "<hyperlink>", "correct": false}
+      ]
+    },
+    {
+      "id": 5,
+      "question": "In CSS, which property is used to set the text color of an element?",
+      "options": [
+        {"option": "color", "correct": true},
+        {"option": "text-color", "correct": false},
+        {"option": "font-color", "correct": false},
+        {"option": "style-color", "correct": false}
+      ]
+    }
+  ];
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await fetchData('./data/questions.json');
+  //       const data = await response.json();
+
+  //       setQuestions(data);
+  //     } catch (error) {
+  //       console.error('Error fetching questions', error);
+  //     }
+  //   }
+
+  // }, []);
+
+const [currentQuestion, setCurrentQuestion] = useState([3])
+
+
   return (
-    <div>
-      <h1>React Quiz App</h1>
-      <Quiz questions={questions} />
+    <div className="grid place-content-center h-screen">
+      <p className="text-center text-5xl m-6">{data[currentQuestion].question}</p>
+      <div className="grid grid-cols-2 gap-2">
+        
+        {data[currentQuestion].options.map((options) => 
+        <button className="border border-stone-700 p-5 rounded-xl">{options.option}</button>        
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/components/Quiz.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Quiz = (data) => {
+  return (
+    <div>
+      <h2>Question</h2>
+      <ul>
+        {}
+      </ul>
+    </div>
+  )
+}
+
+export default Quiz
