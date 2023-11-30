40554c1439f750bfef01980c72a62304498a7613
Gilbert Tan <57788135+gbtan1991@users.noreply.github.com>
2023-11-30 15:45:33 +0800

    Add the changing questions via useState

--- src/App.jsx
+++ src/App.jsx
@@ -70,16 +70,23 @@ export default function App() {
 
   // }, []);
 
-const [currentQuestion, setCurrentQuestion] = useState([3])
+const [currentQuestion, setCurrentQuestion] = useState(0)
+
+const handleAnswerButtonOnClick = () => {
+    const nextQuestion = currentQuestion + 1;
+    setCurrentQuestion(nextQuestion);
+}
 
 
   return (
+    
     <div className="grid place-content-center h-screen">
+      <h3>{`Question ${currentQuestion + 1}  / ${data.length}`}</h3>
       <p className="text-center text-5xl m-6">{data[currentQuestion].question}</p>
       <div className="grid grid-cols-2 gap-2">
-        
+
         {data[currentQuestion].options.map((options) => 
-        <button className="border border-stone-700 p-5 rounded-xl">{options.option}</button>        
+        <button className="border border-stone-700 p-5 rounded-xl" onClick={handleAnswerButtonOnClick}>{options.option}</button>        
         )}
       </div>
     </div>
